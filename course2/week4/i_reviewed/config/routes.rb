696f470db4d602826311e9e97b8b67a0c5b65098
Wilson Valencia <wvalencia@tappsi.co>
2018-07-20 12:26:49 -0500

    Add example app

---
+++ course2/week4/i_reviewed/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :books do
+    resources :notes, only: [:create, :destroy]
+  end
+  root to: "books#index"
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
+end
